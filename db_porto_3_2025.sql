cb766bcc9fc86a5c0da33b40c204498784487ef1
raihanelsar <44558995+raihanelsar@users.noreply.github.com>
2025-08-13 11:11:37 +0700

    update db

--- db_porto_3_2025.sql
+++ db_porto_3_2025.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 11, 2025 at 03:07 AM
+-- Generation Time: Aug 13, 2025 at 06:11 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_porto_3_2025`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+
+CREATE TABLE `about` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `content` text NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `about`
+--
+
+INSERT INTO `about` (`id`, `title`, `content`, `image`, `is_active`, `created_at`, `updated_at`) VALUES
+(2, 'Gunung Bromo', '<p>Gunung Bromo atau dalam bahasa Tengger dieja \"Brama\", juga disebut Kaldera Tengger, adalah sebuah gunung berapi aktif di Jawa Timur, Indonesia.</p>', '1755056982-bromo2.jpg', 1, '2025-08-13 03:49:42', '2025-08-13 04:09:44');
 
 -- --------------------------------------------------------
 
@@ -41,6 +64,37 @@ CREATE TABLE `settings` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `email`, `phone`, `address`, `logo`, `twitter`, `fb`, `ig`, `linkedin`, `created_at`, `updated_at`) VALUES
+(1, 'udin@gmail.com', '086521312434', 'jakarta', '1754966464-burgerking.png', 'https://x.com/', 'https://www.facebook.com/', 'https://www.instagram.com/', 'https://id.linkedin.com/', '2025-08-11 07:54:36', '2025-08-12 02:41:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sliders`
+--
+
+CREATE TABLE `sliders` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sliders`
+--
+
+INSERT INTO `sliders` (`id`, `title`, `description`, `image`, `created_at`, `updated_at`) VALUES
+(16, 'midnight', 'midnight', '1754984010-d197f3d63e273b6f5c105412b799eae6.jpeg', '2025-08-12 07:33:30', NULL),
+(20, 'sky', 'langit', '1755050066-wallpapersden.com_minimal-hd-landscape_1920x1080.jpg', '2025-08-13 01:54:26', NULL),
+(22, 'sunset', 'samsat', '1755050127-1920x1080-aesthetic-glrfk0ntspz3tvxg.jpg', '2025-08-13 01:55:27', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -61,16 +115,29 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin@gmail.com', '12345678', '2025-08-08 07:11:50', '2025-08-09 07:08:14');
+(1, 'Admin', 'admin@gmail.com', '12345678', '2025-08-08 07:11:50', '2025-08-09 07:08:14'),
+(2, 'Elsar', 'elsar@gmail.com', '123456789', '2025-08-11 03:02:28', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `about`
+--
+ALTER TABLE `about`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `settings`
 --
 ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sliders`
+--
+ALTER TABLE `sliders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -84,16 +151,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `about`
+--
+ALTER TABLE `about`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `sliders`
+--
+ALTER TABLE `sliders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
